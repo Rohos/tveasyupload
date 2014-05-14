4b96dc7296b1b7b5da9753f1d7c79c805d51a1cd
Руслан <Deepviz@mail.ru>
2014-05-14 19:52:45 +0400

    Add Russian lexicon
    Add Russian lexicon

---
+++ core/components/tveasyupload/lexicon/ru/default.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$_lang['tveasyupload'] = 'Simple File Input';
+$_lang['easyupload'] = 'Simple File Input';
+
+
+
+// TV Input Properties
+$_lang['tveasyupload.save_path'] = 'Сохранить путь';
+$_lang['tveasyupload.save_path_desc'] = 'Сохранить путь относительно MediaSource корня';
+$_lang['tveasyupload.file_prefix'] = 'Префикс файла';
+$_lang['tveasyupload.file_prefix_desc'] = 'Дополнительный префикс имени файла для загружаемых файлов';
+$_lang['tveasyupload.mime_types'] = 'Допустимые MIME-типы';
+$_lang['tveasyupload.mime_types_desc'] = 'Дополнительный список разделенных запятыми принятых типов MIME';
+$_lang['tveasyupload.show_value'] = 'Показывать TV значение';
+$_lang['tveasyupload.show_value_desc'] = 'Отображать URL к файлу';
+$_lang['tveasyupload.show_preview'] = 'Показывать превью';
+$_lang['tveasyupload.show_preview_desc'] = 'Отображать миниатюры файлов изображений';
+
+// TV Render 
+$_lang['tveasyupload.upload_file'] = 'Загрузить файл...';
+$_lang['tveasyupload.replace_file'] = 'Заменить файл...';
+$_lang['tveasyupload.clear_file'] = 'Удалить значение';
+
+// Errors
+$_lang['tveasyupload.error_tvid_ns'] = 'СИСТЕМНАЯ ОШИБКА: modTemplateVar ID не существует';
+$_lang['tveasyupload.error_tvid_invalid'] = 'СИСТЕМНАЯ ОШИБКА: не корректный modTemplateVar';
+
+$_lang['tveasyupload.err_file_ns'] = 'Ошибка: файл не загружен';
